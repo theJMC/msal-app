df13590593b642be0c80bed3b9edc5f0531033b8
TheJMC <james@jamesmcc.co.uk>
2024-08-09 10:41:48 +0000

    Removed Env Variable debugging

--- v3/src/pages/App.js
+++ v3/src/pages/App.js
@@ -5,8 +5,6 @@ import { useGlobalState } from '../globalState';
 export default function App() {
   const [globalState, ] = useGlobalState()
 
-  console.log(process.env.REACT_APP_CLIENT_ID)
-
   useEffect(() => {
     document.title = globalState.meta.title
   })
